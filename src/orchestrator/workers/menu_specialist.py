10546ad4f811cf6561a3ee6663261523a525005b
ELIZAN <eli.j.alvarez@gmail.com>
2026-04-06 22:47:57 -0600

    feat: complete end to end desired flow

--- src/orchestrator/workers/menu_specialist.py
+++ src/orchestrator/workers/menu_specialist.py
@@ -1,18 +1,23 @@
 from langchain_core.messages import SystemMessage
 from src.orchestrator.state import DeliveryState
 from src.orchestrator.tools.menu_tools import get_menu
+from src.core.prompts import MENU_SPECIALIST_PROMPT
 from typing import Dict, Any
 
 def menu_specialist_node(state: DeliveryState) -> Dict[str, Any]:
     """
-    Nodo especializado en consultas de menú.
-    Accede a la herramienta get_menu e inyecta la respuesta en el estado.
+    Nodo especializado en consultas de menu.
+    Accede a la herramienta get_menu, adjunta las instrucciones de ventas
+    y ejecuta la respuesta en el estado para que el LLM la lea.
     """
     # llamamos la herramienta que conecta a RDS
     menu_information = get_menu.invoke({})
+
+    # Combinamos el System Prompt de ventas con Datos de BD
+    prompt_completo = f"{MENU_SPECIALIST_PROMPT}\n\n--- INFORMACIÓN DE LA BASE DE DATOS ---\n{menu_information}"
     
     # intectamos informacion como un mensaje de sistema en el estado
     # para que cuando usemos add_messages en el state, este mensaje se suma al historial
     return {
-        "messages": [SystemMessage(content=f"Información de la base de datos para el agente:\n{menu_information}")]
+        "messages": [SystemMessage(content=prompt_completo)]
     }
